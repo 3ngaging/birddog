c79e4d4c518075be3f340e22fea9a6d235755136
IagoOrmon <bcniago@gmail.com>
2024-03-26 21:46:13 +0100

    feat(Nav): Style nav

--- src/app/components/nav.js
+++ src/app/components/nav.js
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import Image from "next/image";
-import tgImg from '../files/telegram.svg';
-import xImg from '../files/square-x-twitter.svg';
+import tgImg from '../assets/tg_yellow.svg';
+import xImg from '../assets/x_yellow.svg';
 
 const Nav = () => {
   return (
@@ -10,29 +10,29 @@ const Nav = () => {
         {/* Left side navigation */}
         <ul className="flex space-x-4">
           <li>
-            <Link href="/" className="hover:text-gray-300">Home</Link>
+            <Link href="/" className="hover:text-gray-50 bg-orange-400 hover:bg-yellow-800 p-2 px-5 border-white-400 rounded-3xl border-2">Home</Link>
           </li>
           <li>
-            <Link href="/meme-generator" className="hover:text-gray-300">Meme Generator</Link>
+            <Link href="/meme-generator" className="hover:text-gray-50 bg-orange-400 hover:bg-yellow-800 p-2 px-5 border-white-400 rounded-3xl">Meme Generator</Link>
           </li>
           <li>
             {/* "Coming Soon..." button without a path */}
-            <span className="cursor-not-allowed hover:text-gray-300">Coming Soon...</span>
+            <span className="cursor-not-allowed text-yellow-800 bg-orange-400 p-2 px-5 border-white-400 rounded-3xl">Coming Soon...</span>
           </li>
         </ul>
 
         {/* Right side buttons */}
         <div className="flex items-center space-x-4">
           {/* Placeholder for social media icons */}
-          <a href="https://t.me/birddogentry" className="text-yellow-500 hover:text-gray-400">
+          <a href="https://t.me/birddogentry" className="hover:opacity-75">
             <Image src={tgImg} alt="telegram logo" priority height={"32"}/>
           </a>
-          <a href="https://twitter.com/SOLbirddog" className="text-yellow-500 hover:text-gray-400">
+          <a href="https://twitter.com/SOLbirddog" className="hover:opacity-75">
             <Image src={xImg} alt="x logo" priority height={"32"}/>
           </a>
 
           {/* BUY NOW button */}
-          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          <button className="hover:text-gray-50 bg-orange-400 hover:bg-yellow-800 p-2 px-5 border-yellow-400 rounded-3xl border-4 text-xl">
             BUY NOW
           </button>
         </div>
