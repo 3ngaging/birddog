d2141e59c66e7bdd449331dd225e27889c1dbbae
IagoOrmon <bcniago@gmail.com>
2024-03-26 21:22:32 +0100

    feat(Router): Update routers

---
+++ src/app/meme-generator/page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+// Import other required components or hooks
+
+export default function MemeGenerator() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between px-5 bg-orange-500">
+      <h1>Meme Generator</h1>
+      {/* Your meme generator content goes here */}
+    </main>
+  );
+}
