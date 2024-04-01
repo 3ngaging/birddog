b5f397d02203ab23a8ae5f6c3859ae9d348515e7
IagoOrmon <bcniago@gmail.com>
2024-04-02 00:10:19 +0200

    Update footer.js

--- src/app/components/footer.js
+++ src/app/components/footer.js
@@ -9,7 +9,7 @@ const myFont2 = localFont2({ src: "../files/ComicNeue-Bold.ttf" });
 const Footer = () => {
   return (
     <>
-      <div className="bg-black flex flex-col sm:flex-row justify-center px-4 sm:px-10 gap-4 sm:gap-10 z-10">
+      <div className="bg-black flex flex-col sm:flex-row justify-center px-4 sm:px-10 gap-4 sm:gap-10 z-10 text-white">
         <div className="flex items-center justify-center sm:justify-start gap-5">
           <a
             href="https://t.me/birddogentry"
@@ -54,7 +54,7 @@ const Footer = () => {
           />
         </div>
       </div>
-      <footer className="bg-stone-950 p-4">
+      <footer className="bg-stone-950 p-4 text-white">
         <div className="container mx-auto text-center">
           <p className="from-purple-500 to-emerald-400 bg-gradient-to-r bg-clip-text text-transparent">© 2024 BIRDDOG COMMUNITY. All rights reserved.</p>
           <p className={myFont2.className}>Website powered by <a className="underline hover:text-purple-500" href="https://twitter.com/3ngaging" target="_blank">@3ngaging</a> Developer.</p>
